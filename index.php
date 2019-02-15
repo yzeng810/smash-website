7692b1bbd9ecac74b92f638ed3116eefc96db5d0
Yuqi Zeng <yuqizeng@YuqideMacBook.local>
2019-02-15 02:47:09 -0500

    update

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
 
 <section class="container">
 
+	<h2>Intro</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur.</p>
+
 	<div class="slider">
 			<img class="flexible" src="images/Politician.jpg" alt="Politician">
 			<img class="flexible" src="images/pl.jpg" alt="Pianists">
